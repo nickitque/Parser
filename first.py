b0e34cadce4c8586cdbd5d9183e19adcfeabb686
nickitque <58918756+nickitque@users.noreply.github.com>
2021-03-18 23:13:27 +0300

    Update

--- first.py
+++ first.py
@@ -1,9 +1,10 @@
+
 import requests
- 
-r = requests.get('https://google.com')
- 
-if r.status_code == 200:
-    print('OK!')
- 
-if r.status_code == 404:
-    print('Not found')
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+htmlpage = requests.get("https://rabota.by/search/vacancy?area=1002&fromSearchLine=true&st=searchVacancy&text=python&page=0", headers=headers)
+
+soup = BeautifulSoup(htmlpage.text, 'html.parser')
+
+print(soup)
