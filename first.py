41ada9c382577484400f7b9857626f90e4defc0a
nickitque <58918756+nickitque@users.noreply.github.com>
2021-03-19 10:17:06 +0300

    Adding variable of the page

--- first.py
+++ first.py
@@ -2,12 +2,14 @@
 
 #first part: getting html code of the page
 
+import re
 import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-htmlpage = requests.get("https://rabota.by/search/vacancy?area=1002&fromSearchLine=true&st=searchVacancy&text=python&page=0", headers=headers)
+page = 0
 
+htmlpage = requests.get("https://rabota.by/search/vacancy?area=1002&fromSearchLine=true&st=searchVacancy&text=python&page={str{page}}", headers=headers)
 soup = BeautifulSoup(htmlpage.text, 'html.parser')
 
 #second part: extracting links of vacancies
